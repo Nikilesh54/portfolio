888dacf45ab2116f1bb8f44294139b74b7956d0a
Nikilesh54 <108801475+Nikilesh54@users.noreply.github.com>
2025-01-09 09:30:50 -0500

    Resume Button

--- src/Home.tsx
+++ src/Home.tsx
@@ -105,10 +105,21 @@ const AboutSection: React.FC = () => {
                     <a
                         rel="noreferrer"
                         target="_blank"
-                        className="cta-btn cta-btn--resume"
-                        href="/resume.pdf"
+                        className="botao"
+                        href="/Nikilesh%20Madala%20Resume.pdf"
                     >
-                        View Resume
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none"
+                             xmlns="http://www.w3.org/2000/svg" className="mysvg">
+                            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <g id="Interface / Download">
+                                    <path id="Vector" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" stroke="#f1f1f1"
+                                          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                </g>
+                            </g>
+                        </svg>
+                        <span className="texto">View Resume</span>
                     </a>
                 </div>
             </div>
